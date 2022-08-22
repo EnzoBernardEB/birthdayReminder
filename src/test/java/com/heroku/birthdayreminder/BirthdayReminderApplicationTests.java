3223cb15d5a66b6e9f99620c1d2e2abd89364783
EnzoBernardEB <68954967+EnzoBernardEB@users.noreply.github.com>
2022-08-22 12:02:40 +0200

    [INIT] Initalisation projet

---
+++ src/test/java/com/heroku/birthdayreminder/BirthdayReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.birthdayreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
